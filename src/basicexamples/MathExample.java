10bb491f85b8f8331f0274640821b72a31c3931c
radvanisko <radvanisko@users.noreply.github.com>
2023-01-04 21:23:27 +0100

    doplnenie

---
+++ src/basicexamples/MathExample.java
@@ -0,0 +1,14 @@
+package basicexamples;
+
+public class MathExample {
+
+    public static void main(String[] args) {
+        double maxValue=Math.max(5.10,10);
+        System.out.println(maxValue);
+        
+        
+        int randomValue= (int) (Math.random()*101);
+        System.out.println(randomValue);
+
+    }
+}
